5fa9945410137560291ef60ff45f78504d4dadc2
Elizabeth <deuzm.github.com>
2020-02-17 11:53:18 +0300

    Initial Commit

---
+++ NoteAntidote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoteAntidote
+//
+//  Created by Лиза  Малиновская on 2/17/20.
+//  Copyright © 2020 Лиза  Малиновская. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

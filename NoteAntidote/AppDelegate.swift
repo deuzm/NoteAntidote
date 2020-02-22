9513ef2c714ca0547b9a55ec14ccc702690853c8
Elizabeth <deuzm.github.com>
2020-02-22 08:48:16 +0300

    added more missing items

--- NoteAntidote/AppDelegate.swift
+++ NoteAntidote/AppDelegate.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 
+let window = UIWindow(frame: UIScreen.main.bounds)
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions
+        launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        //MARK: - if it doesen't work delete it
         return true
     }
 
@@ -32,6 +35,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
 }
-
--- NoteAntidote/RootViewController.swift
+++ NoteAntidote/RootViewController.swift
@@ -8,15 +8,120 @@
 
 import UIKit
 
-class RootViewController: UIViewController {
+protocol TasksProtocol
+{
+    func tasksAssigned(type: [Task])
+}
 
+class RootViewController: UITableViewController {
+
+    
+
+    //MARK: - properties
+    var tasks: [Task] = []
+    var card: Card = Card(tasks: [], cardTitleText: "Default", cardDescription: "Default")
+    var cardData = CardData()
+    var firstCell = true
+    var cardSetterVC: CardSetterViewController?
+    var delegate:TasksProtocol?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.allowsSelection = false
+        cardSetterVC = CardSetterViewController()
+        
         // Do any additional setup after loading the view.
     }
     
+//    required init?(coder aDecoder: NSCoder) {
+//        super.init(coder: aDecoder)
+//    }
+//    
 
+    //MARK: - outlets
+//    @IBOutlet weak var taskTitle: UILabel!
+//    @IBOutlet weak var taskTextField: UITextField!
+//    @IBOutlet weak var taskCell: UIView!
+//
+//    MARK: - actions
+
+    
+    
+    @IBAction func taskTextFieldEditingFinished(_ sender: Any) {
+        cardSetterVC?.completionHandler = { self.tasks }
+        let textField = sender as! UITextField
+        var a = "lol"
+        if(textField.text != "" && textField.text != nil)
+        {
+            //TODO
+            
+            let cardId = cardSetterVC?.cardId
+            tasks.append(Task(cardId: cardId ?? 0, taskId: tasks.count + (cardId ?? 0), title: "\(textField.text ?? a)"))
+            
+           
+            tableView.beginUpdates()
+
+            tableView.insertRows(at: [IndexPath(row: tasks.count, section: 0)], with: .top)
+            tableView.endUpdates()
+            
+            textField.text = ""
+//            tableView.beginUpdates()
+//            tableView.deleteRows(at: [IndexPath(row: 0, section: 0)], with: .automatic)
+//            tableView.endUpdates()
+            
+            print(tasks[tasks.count - 1].titleText)
+            
+            delegate?.tasksAssigned(type: tasks)
+            
+        }
+        
+    }
+    
+//    @IBAction func taskEditingDIdEnd(_ sender: Any) {
+//        taskTitle.text = taskTextField.text
+//        taskTextField.isHidden = true
+//    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count + 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if(firstCell || indexPath.row == 0)
+        {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "editor", for: indexPath)
+            firstCell = false
+            cell.setEditing(false, animated: true)
+            return cell
+        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCellId", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row - 1].titleText
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        if indexPath.row == tasks.count
+        {
+            return UITableViewCell.EditingStyle.none
+        }
+        return UITableViewCell.EditingStyle.delete
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete{
+            tasks.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.automatic)
+        }
+    }
+    
+    //MARK: - Passing items to cardSetter view controller
+    
+
+    
     /*
     // MARK: - Navigation
 
